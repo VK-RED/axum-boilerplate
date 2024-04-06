edb0d4513354e7996e6f1aadfa6db9a4697567a3
Vishnu Kumar <vishnukumart96@gmail.com>
2024-04-06 09:53:56 +0530

    Add BoilerPlate Code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main(){
+    let app = axum::Router::new().route("/", get(hello_world));
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_world() -> String{
+    String::from("hello world")
 }
